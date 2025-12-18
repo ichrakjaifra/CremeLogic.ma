c84ace79b622ab81bbc2e19576a80cf01c15b8ca
ichrakjaifra <ichrak.jaifra@gmail>
2025-12-18 15:24:15 +0100

    SDGIDP-5 Configuration de l'Environnement de Développement

---
+++ backend/src/test/java/ma/cremelogic/CremeLogic/ma/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cremelogic.CremeLogic.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
