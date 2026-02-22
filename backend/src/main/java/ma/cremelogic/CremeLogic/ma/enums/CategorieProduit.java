279ebb9339148b73c0077c7a82033364a082abb3
ichrakjaifra <ichrak.jaifra@gmail>
2026-02-22 17:01:49 +0000

    SDGIDP-17 Gestion des Catégories

---
+++ backend/src/main/java/ma/cremelogic/CremeLogic/ma/enums/CategorieProduit.java
@@ -0,0 +1,12 @@
+package ma.cremelogic.CremeLogic.ma.enums;
+
+public enum CategorieProduit {
+    GATEAUX,
+    PATISSERIES,
+    VIENNOISERIES,
+    BOULANGERIE,
+    DESSERTS,
+    BOISSONS,
+    SANDWICHS,
+    SALADES
+}
